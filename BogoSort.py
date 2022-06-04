cc89fe39530dc18d20d1e4291acf8e468d41fe5d
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2022-06-04 19:38:19 -0400

    Create BogoSort.py

---
+++ BogoSort.py
@@ -0,0 +1,37 @@
+import random, os
+os.system ("cls")
+output = False #Change this to True if you want to output the list if its not sorted yet, decreases performance though (;
+while True:
+    p0 = random.randint(0,5)
+    p1 = random.randint(0,5)
+    p2 = random.randint(0,5)
+    p3 = random.randint(0,5)
+    p4 = random.randint(0,5)
+    p5 = random.randint(0,5)
+    if p0 > p1:
+        g1 = True
+    else:
+        g1 = False
+    if p1 > p2:
+        g2 = True
+    else:
+        g2 = False
+    if p2 > p3:
+        g3 = True
+    else:
+        g3 = False
+    if p3 > p4:
+        g4 = True
+    else:
+        g4 = False
+    if p4 == p5 + 1:
+        g5 = True
+    else:
+        g5 = False
+    if g1 and g2 and g3 and g4 and g5 == True:
+        print (f"Sorted, {p0, p1, p2, p3, p4, p5}")
+        break
+    if output == True:
+        if g1 or g2 or g3 or g4 or g5 == False:
+            print (f"Not sorted, {p0, p1, p2, p3, p4, p5}")
+            #os.system ("cls") #Uncomment this if you want to clear the screen each time, decreases performance though (;
